b6daaf83290a0b2a7dce2d669b5d88b4cbc32b1f
Elizabeth <namelesselizabeth@gmail.com>
2023-07-27 05:27:10 -0700

    v2
    Cleaned up html & css

--- src/main/java/com/coderscampus/Elizabeth_Assignment_13/web/AccountController.java
+++ src/main/java/com/coderscampus/Elizabeth_Assignment_13/web/AccountController.java
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.coderscampus.Elizabeth_Assignment_13.domain.Account;
+import com.coderscampus.Elizabeth_Assignment_13.domain.User;
 import com.coderscampus.Elizabeth_Assignment_13.service.AccountService;
+import com.coderscampus.Elizabeth_Assignment_13.service.UserService;
 
 @Controller
 public class AccountController {
 
 	@Autowired
 	private AccountService accountService;
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("/users/{userId}/accounts/{accountId}")
 	public String getAccount(ModelMap model, @PathVariable Long accountId) {
@@ -22,7 +26,7 @@ public class AccountController {
 		model.put("accounts", account);
 		return "accounts";
 	}
-//
+
 	@PostMapping("/users/{userId}/accounts/{accountId}")
 	public String postAccount(Account account, @PathVariable Long userId, @PathVariable Long accountId) {
 		account.setAccountId(accountId);
@@ -30,6 +34,15 @@ public class AccountController {
 		return "redirect:/users/" + userId + "/accounts/" + account.getAccountId();
 	}
 	
-//	@GetMapping("/users/{userId}/accounts")
-//	public String getAllAccounts ()
+	@PostMapping("/users/{userId}/accounts")
+	public String createAccount(@PathVariable Long userId) {
+		User user = userService.findById(userId);
+		Account account = new Account();
+		account.setAccountName("Account #" + user.getAccounts().size());
+		user.getAccounts().add(account);
+		account.getUsers().add(user);
+		accountService.saveAccount(account);
+		
+		return "redirect:/users/" + userId;
+	}
 }
